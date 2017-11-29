59d9efa96be020cb7075cdfdf1cb5d7854bf3b0f
Gennaro Anzalone and Mitch Long <mitch@>
2017-11-29 10:24:57 -0800

    Pass first test, factorybot working

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 RSpec.describe Album, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe Album do
+  it 'validates presence of title' do
+    album = FactoryBot.create(:album, :title => "Flatstick pub")
+    album.title.should eq "Flatstick pub"
+  end
+end
