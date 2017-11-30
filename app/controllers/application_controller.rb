e1c06656967d6d89e9898609be8a5e97af234792
Gennaro Anzalone and Mitch Long <gennaro@>
2017-11-30 12:59:26 -0800

    fix authentication error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :require_login
+  before_action :authenticate_user!
 
-  private
-
- def require_login
-   unless !current_user
-     flash[:error] = "You must be logged in to access this section"
-     redirect_to '/users/sign_in' # halts request cycle
-   end
- end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :require_login, only: [:new]
+  skip_before_action :authenticate_user!, :only => [:home]
 
   def home
   end
