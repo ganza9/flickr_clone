89b8a7ac500a6f4c0573269b780887b7e5b085fd
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-11-30 14:23:54 -0800

    nest routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
 
   devise_for :users
-  resources :photos
+
   resources :albums
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :photos
+  end
+
+  resources :photos do
+    resources :comments
+  end
+
 end
