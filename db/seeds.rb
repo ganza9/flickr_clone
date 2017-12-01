c7dcf473914f96a7aab03f633616d91544a0bb4a
Mitchell <meechllada@gmail.com>
2017-11-30 16:37:53 -0800

    Attempt to fix image attatchment

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,16 @@
 #
 # 50.times do |index|
 #   Photo.create!(title: Faker::Seinfeld.character,
-#                         description:Faker::Lorem.paragraph,
-#                         album_id:Faker::Number.between(1, 50),
-#                         comment_id:Faker::Number.between(1, 50),
+#                         description: Faker::Lorem.paragraph,
+#                         album_id: Faker::Number.between(1, 50),
+#                         image: Faker::Fillmurray.image(true)
 #                         )
 # end
 #
 # p "Created #{Photo.count} photos"
+
+title1 = "Mt. Rainier"
+description1 = "Amazing Mountain"
+image_file_name1 = "assests/images/rainier.jpg"
+
+Photo.create(title: title1, description: description1, image_file_name: image_file_name1 )
